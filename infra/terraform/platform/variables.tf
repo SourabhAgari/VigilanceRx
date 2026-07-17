58461d3e18087aabada59e3f50538009cc9700d6
sourabhragari <agarisourabh@gmail.com>
2026-07-17 11:02:32 +0530

    added terraform zone variable

--- infra/terraform/platform/variables.tf
+++ infra/terraform/platform/variables.tf
@@ -9,3 +9,9 @@ variable "region" {
   type        = string
   default     = "us-central1"
 }
+
+variable "zone" {
+  description = "GCP zone for the zonal GKE cluster (D7: single zone for cost)"
+  type        = string
+  default     = "us-central1-a"
+}
