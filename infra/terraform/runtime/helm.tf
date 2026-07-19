9aaee9542840af23cd2ad8c1d59a40afb6b646e9
sourabhragari <agarisourabh@gmail.com>
2026-07-19 08:11:49 +0530

    added flink operator helm repo

--- infra/terraform/runtime/helm.tf
+++ infra/terraform/runtime/helm.tf
@@ -13,3 +13,16 @@ resource "helm_release" "cert_manager" {
     }
   ]
 }
+
+resource "helm_release" "flink_operator" {
+  name             = "flink-kubernetes-operator"
+  repository       = "https://downloads.apache.org/flink/flink-kubernetes-operator-1.15.0/"
+  chart            = "flink-kubernetes-operator"
+  version          = "1.15.0"
+  namespace        = "flink-system"
+  create_namespace = true
+
+  # No attribute ties these releases together, but the operator's admission
+  # webhook needs cert-manager ready to issue its TLS cert (§10 chain).
+  depends_on = [helm_release.cert_manager]
+}
