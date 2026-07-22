6bc6c21969ffea6f318fb2bee9cf0499558c335f
sourabhragari <agarisourabh@gmail.com>
2026-07-22 08:53:20 +0530

    added comments

--- src/main/java/com/healthcare/rxvigilance/domain/CoverageInterval.java
+++ src/main/java/com/healthcare/rxvigilance/domain/CoverageInterval.java
@@ -3,8 +3,6 @@ package com.healthcare.rxvigilance.domain;
 import java.time.LocalDate;
 
 public record CoverageInterval(String claimId, LocalDate start,LocalDate end) {
-
-
     // customizing the constructor behavior
     public CoverageInterval {
         if (start.isAfter(end)) {
