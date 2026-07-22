21ec741d66fc56bf4fb244b3a96f0f7d099cdb29
sourabhragari <agarisourabh@gmail.com>
2026-07-22 08:21:56 +0530

    added coverage interval

--- src/main/java/com/healthcare/rxvigilance/domain/CoverageInterval.java
+++ src/main/java/com/healthcare/rxvigilance/domain/CoverageInterval.java
@@ -1,4 +1,15 @@
 package com.healthcare.rxvigilance.domain;
 
-public record CoverageInterval() {
+import java.time.LocalDate;
+
+public record CoverageInterval(String claimId, LocalDate start,LocalDate end) {
+
+
+    // customizing the constructor behavior
+    public CoverageInterval {
+        if (start.isAfter(end)) {
+            throw new IllegalArgumentException(
+                    "CoverageInterval start (" + start + ") is after end (" + end + ") for claimId " + claimId);
+        }
+    }
 }
