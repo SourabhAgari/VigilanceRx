3f317b46d9948188c968a4bc640b6363037a0e81
sourabhragari <agarisourabh@gmail.com>
2026-07-25 11:02:48 +0530

    added config object in RxWatermarkStrategy

--- src/main/java/com/healthcare/rxvigilance/watermark/RxFillWatermarkStrategy.java
+++ src/main/java/com/healthcare/rxvigilance/watermark/RxFillWatermarkStrategy.java
@@ -1,20 +1,20 @@
 package com.healthcare.rxvigilance.watermark;
 
+import com.healthcare.rxvigilance.config.WatermarkConfig;
 import com.healthcare.rxvigilance.domain.RxFillEvent;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 
-import java.time.Duration;
 import java.time.ZoneOffset;
 
 public final class RxFillWatermarkStrategy {
     private RxFillWatermarkStrategy() {
     }
 
-    public static WatermarkStrategy<RxFillEvent> create() {
+    public static WatermarkStrategy<RxFillEvent> create(WatermarkConfig config) {
         return WatermarkStrategy
-                .<RxFillEvent>forBoundedOutOfOrderness(Duration.ofHours(24))
+                .<RxFillEvent>forBoundedOutOfOrderness(config.outOfOrderness())
                 .withTimestampAssigner((event, recordTimestamp) ->
                         event.fillDate().atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli())
-                .withIdleness(Duration.ofMinutes(5));
+                .withIdleness(config.idleness());
     }
 }
