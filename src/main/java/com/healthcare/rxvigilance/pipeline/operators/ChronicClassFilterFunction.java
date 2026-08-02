350de4321c37703b35490abb4c86351f6ee1a84e
sourabhragari <agarisourabh@gmail.com>
2026-08-02 20:26:38 +0530

    removing isEmpty method

--- src/main/java/com/healthcare/rxvigilance/pipeline/operators/ChronicClassFilterFunction.java
+++ src/main/java/com/healthcare/rxvigilance/pipeline/operators/ChronicClassFilterFunction.java
@@ -71,9 +71,9 @@ public class ChronicClassFilterFunction
         collector.collect(new EnrichedFillEvent(event,ref.drugClass()));
     }
 
-    private static boolean isEmpty(ReadOnlyBroadcastState<String,DrugClassRef> state) throws Exception {
-        return  !state.immutableEntries().iterator().hasNext();
-    }
+//    private static boolean isEmpty(ReadOnlyBroadcastState<String,DrugClassRef> state) throws Exception {
+//        return  !state.immutableEntries().iterator().hasNext();
+//    }
 
     public void processBroadcastElement(DrugClassRefUpdate drugClassRefUpdate, Context context, Collector<EnrichedFillEvent> collector) throws Exception {
         BroadcastState<String, DrugClassRef> broadcastState = context.getBroadcastState(NDC_CLASS_DESCRIPTOR);
